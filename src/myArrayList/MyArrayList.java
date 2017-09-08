dab955501c05ca45544ef49e5316cf2434c67669
tushitjain <tushitjain@gmail.com>
2017-09-08 14:06:42 -0400

    myArrayList class all functions implemented

--- src/myArrayList/MyArrayList.java
+++ src/myArrayList/MyArrayList.java
@@ -41,6 +41,51 @@ public class MyArrayList {
 		newArray = initializeArray(newArray,size);
 		array = newArray;
 	}
+	
+	public int indexOf(int value){
+		for(int i=0;i<size;i++){
+			if(array[i]==value){
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	public int sum(){
+		int sum=0;
+		for(int i=0;i<size;i++){
+			sum = sum + array[i];
+		}
+		return sum;
+	}
+	
+	public int size(){
+		return size;
+	}
+	public void removeValue(int value){
+		int index;
+		int count=0;
+		while((index = indexOf(value)) > -1){
+			count++;
+			array[index] = Integer.MAX_VALUE;
+			size--;
+		}
+		if(count>0)
+			sortMyArrayList();
+		else
+			System.out.println("Int value not found.");
+	}
+	
+	@Override
+	public String toString(){
+		StringBuilder sb = new StringBuilder();
+		for(int i=0 ; i<size ;i++){
+			String str = array[i] + " ";
+			sb.append(str);
+		}
+		return sb.toString();
+	}
+	
 	public static void main(String[] args) {
 		MyArrayList arr = new MyArrayList();
 		arr.insertSorted(10);
@@ -57,13 +102,23 @@ public class MyArrayList {
 		arr.insertSorted(100);
 		arr.insertSorted(13);
 		arr.insertSorted(167);
-		arr.insertSorted(1111);
+		arr.insertSorted(7);
 		arr.insertSorted(11);
-		arr.insertSorted(12);
+		arr.insertSorted(5);
 		arr.insertSorted(2);
 		arr.insertSorted(23);
-		for(int i=0;i<arr.size;i++)
-			System.out.println(arr.array[i]);
+		
+		System.out.println("Index of 5 is : " +arr.indexOf(5));
+		System.out.println("Size is : "+arr.size());
+		System.out.println("Sum is "+arr.sum());
+		
+		arr.removeValue(7);
+		arr.removeValue(11);
+		System.out.println("after remove");
+		System.out.println("Index of 5 is : " +arr.indexOf(5));
+		System.out.println("Size is : "+arr.size());
+		System.out.println("Sum is "+arr.sum());
+		System.out.println(arr.toString());
 	}
 
 }
